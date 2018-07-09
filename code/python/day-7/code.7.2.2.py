7aa3d302486a94106d7f920eeb1be2d2e5cae14c
kunaly1038 <39200197+kunaly1038@users.noreply.github.com>
2018-07-10 01:52:26 +0530

    Update code.7.2.2.py

--- code/python/day-7/code.7.2.2.py
+++ code/python/day-7/code.7.2.2.py
@@ -1,18 +1,14 @@
 def fun():
     arr = []
     duplicatesubarray=[]
-    key = 0
-    x = int(input('How many numbers you want to enter in Arrya :'))
+    x = int(input('How many numbers you want to enter in Array :'))
     for i in range(x):
-        xy = int(input('Enter the number into the arrya :'))
+        xy = int(input('Enter the number into the array :'))
         arr.append(xy)
-    
     for i in range(len(arr)):
         for j in range(len(arr)):
             if(arr[j]==arr[i] and i!=j):
                 duplicatesubarray.append(arr[j])    
-                break
-            
+                break    
     print(duplicatesubarray)
-
 fun()
