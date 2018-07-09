f6473f32784229434e1bd7b58faeaf546d050886
kunaly1038 <39200197+kunaly1038@users.noreply.github.com>
2018-07-10 01:51:27 +0530

    Update code.7.2.1.py

--- code/python/day-7/code.7.2.1.py
+++ code/python/day-7/code.7.2.1.py
@@ -1,18 +1,13 @@
 def fun():
     arr = []
-    subarray=[]
-    key = 0
-    x = int(input('How many numbers you want to enter in Arrya :'))
+    x = int(input('How many numbers you want to enter in Array :'))
     for i in range(x):
         xy = int(input('Enter the number into the arrya :'))
         arr.append(xy)
-    
     for i in range(len(arr)):
         for j in range(len(arr)):
             if(arr[j]==arr[i] and i!=j):
                 duplicate=arr[j]
                 break
-            
     print(duplicate)
-
 fun()
